80c36e4177b33c206932cd5db16a174ef5f8dca1
Leon Bowie <leon@bowie-co.nz>
2021-10-14 01:05:26 +1300

    initial commit

---
+++ src/commands/Moderation/kick.ts
@@ -0,0 +1,21 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Args, Command, CommandOptions } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+@ApplyOptions<CommandOptions>({
+    description: 'kick a user',
+    preconditions: ['Moderator'],
+})
+export class UserCommand extends Command {
+    public async run(message: Message, args: Args) {
+        const user = await args.rest('member');
+        const reason = await args.rest('string', { default: `No reason provided. (${message.author.tag})`, minimum: 0 });
+        if (user.id === message.author.id) return message.channel.send('You cannot kick yourself.');
+        if (user.id === message.guild!.ownerId) return message.channel.send('You cannot kick the server owner.');
+        if (user.id === message.guild!.me!.id) return message.channel.send('You cannot kick the bot.');
+        if (user.roles.highest.position >= message.member!.roles.highest.position) return message.channel.send('You cannot kick someone with a higher or equal role.');
+        if (!user.kickable) return message.channel.send('I cannot kick this user.');
+        await message.react('✅');
+        return await user.kick(reason);
+    }
+}
---
+++ src/preconditions/Administrator.ts
@@ -0,0 +1,18 @@
+import { Precondition } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+export class UserPrecondition extends Precondition {
+    public async run(message: Message) {
+        if (!message.guild) {
+            return this.error({ message: 'This cannot be run in dms' });
+        }
+        return message.member!.permissions.has('ADMINISTRATOR') ? this.ok() : this.error({ message: 'This command can only run by Administrators!' });
+    }
+}
+
+
+declare module '@sapphire/framework' {
+    interface Preconditions {
+        Administrator: never;
+    }
+}
