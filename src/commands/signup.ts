588f62ae3c78a53482b217aa58cfcf61770e6634
Leon Bowie <leon@bowie-co.nz>
2022-11-20 17:48:07 +1300

    fix: move signup to work on all servers

--- src/commands/signup.ts
+++ src/commands/signup.ts
@@ -2,7 +2,7 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
 
 @ApplyOptions<Command.Options>({
-	description: 'Sign up to the Victoria Engineering Club or one of the groups in the Collective.'
+	description: 'Sign up to Uni Clubs.'
 })
 export class UserCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
@@ -15,8 +15,23 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputInteraction) {
+		// get invite code
+		const inviteLink = await this.container.db.guild.findUnique({
+			where: { id: interaction.guildId! },
+			select: { signUpLink: true }
+		});
+
+		if (!inviteLink) {
+			await interaction.reply({
+				content:
+					'This server has not set up a sign up link yet or is not a university club. You can search for clubs [here](https://www.wgtn.ac.nz/students/campus/clubs/directory)',
+				ephemeral: true
+			});
+			return;
+		}
+
 		await interaction.reply({
-			content: 'You can signup at to VEC for free at this link [here](https://forms.gle/ooUf1NSCgUJV8h4H9).',
+			content: `You can signup at to ${interaction.guild?.name} for free at this link [here](${inviteLink}). You can also see the full clubs list [here](https://www.wgtn.ac.nz/students/campus/clubs/directory).`,
 			ephemeral: true
 		});
 	}
